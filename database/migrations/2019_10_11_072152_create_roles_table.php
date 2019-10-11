b9002300f2dae8660552afd832be17d966f45b50
aduar242 <aduarjendi1@gmail.com>
2019-10-11 16:02:09 +0700

    fucking ROLE

--- database/migrations/2019_10_11_072152_create_roles_table.php
+++ database/migrations/2019_10_11_072152_create_roles_table.php
@@ -13,10 +13,10 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create(‘roles’, function (Blueprint $table) {
-            $table->increments(‘id’);
-            $table->string(‘name’);
-            $table->string(‘description’);
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
